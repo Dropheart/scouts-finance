c634a81cbe713ff05f51925a8be0fd9f511c76a6
George Decesare <george.decesare@imperial.ic.ac.uk>
2025-05-29 19:44:21 +0100

    Add past events

--- scouts_finances_flutter/lib/events/home.dart
+++ scouts_finances_flutter/lib/events/home.dart
@@ -6,13 +6,13 @@ class EventHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // This will become a map over data
-    List<Card> events = [
+    List<Widget> events = [
       Card(
         child: ListTile(
           title: const Text('Event Name'),
           subtitle: Row(
             children: [
-              const Text('15/27 Paid'),
+              const Text('15/27 paid'),
               const Spacer(),
               const Text('DD/MM'),
             ],
@@ -26,12 +26,12 @@ class EventHome extends StatelessWidget {
 
       Card(
         child: ListTile(
-          title: const Text('Another Event'),
+          title: const Text('August Camp'),
           subtitle: Row(
             children: [
-              const Text('12/12 Paid'),
+              const Text('4/15 paid'),
               const Spacer(),
-              const Text('DD/MM'),
+              const Text('20/06/2025'),
             ],
           ),
           onTap: () {
@@ -40,6 +40,25 @@ class EventHome extends StatelessWidget {
           trailing: const Icon(Icons.arrow_forward),
         ),
       ),
+
+      ExpansionTile(title: const Text('Past Events'), children: [
+        Card(
+          child: ListTile(
+            title: const Text('Winter Camp'),
+            subtitle: Row(
+              children: [
+                const Text('15/20 paid'),
+                const Spacer(),
+                const Text('01/01/2025'),
+              ],
+            ),
+            onTap: () {
+              // Navigate to event details
+            },
+            trailing: const Icon(Icons.arrow_forward),
+          ),
+        ),
+      ]),
     ];
 
     Center body = Center(child: ListView(children: events));
