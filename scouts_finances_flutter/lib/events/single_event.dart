bf33c8a9833ed00d829561e5070a27b27f3fce6b
Jay <jay@abussaud.dev>
2025-05-30 04:06:36 +0100

    feat: basic implementation of event view

--- scouts_finances_flutter/lib/events/single_event.dart
+++ scouts_finances_flutter/lib/events/single_event.dart
@@ -14,16 +14,19 @@ class SingleEvent extends StatefulWidget {
 }
 
 class _SingleEventState extends State<SingleEvent> {
-  late EventDetails eventDetails;
+  late Event event;
+  late List<EventRegistration> registrations;
   String? errorMessage;
   bool loading = true;
 
   void _getEventDetails() async {
     try {
-      final details = await client.event.getEventById(widget.eventId);
+      final (eventRes, registrationRes) =
+          await client.event.getEventById(widget.eventId);
 
       setState(() {
-        eventDetails = details;
+        event = eventRes;
+        registrations = registrationRes;
         loading = false;
       });
     } catch (e) {
@@ -49,13 +52,55 @@ class _SingleEventState extends State<SingleEvent> {
       return Center(child: Text(errorMessage!));
     }
 
+    final children = registrations.map((e) => (
+          childId: e.child!.id,
+          name: "${e.child!.firstName} ${e.child!.lastName}",
+          paid: e.payments!
+              .fold(0.0, (previous, payment) => previous + payment.amount)
+        ));
+
+    final childrenTable = DataTable(
+        columns: [
+          DataColumn(label: Text('Child ID')),
+          DataColumn(label: Text('Name')),
+          DataColumn(label: Text('Paid')),
+        ],
+        rows: children
+            .map((e) => DataRow(
+                    cells: [
+                      DataCell(Text('${e.childId}')),
+                      DataCell(Text(e.name)),
+                      DataCell(Text(e.paid.toStringAsFixed(2))),
+                    ],
+                    color: e.paid < event.cost
+                        ? WidgetStateProperty.all(Colors.red[100])
+                        : WidgetStateProperty.all(Colors.green[100])))
+            .toList());
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Event ${widget.eventId} Details'),
       ),
       body: Center(
-        child: Text(eventDetails.$1.name),
-      ),
+          child: Column(
+        children: [
+          Row(children: [
+            Text('Event: ${event.name}'),
+            Spacer(),
+            Text('Date: ${event.date}'),
+          ]),
+          const SizedBox(height: 10),
+          Row(children: [
+            Text('Location: TBD'),
+            Spacer(),
+            Text('Price: ${event.cost.toStringAsFixed(2)}'),
+          ]),
+          const SizedBox(height: 20),
+          const Text('Registrations:'),
+          const SizedBox(height: 10),
+          childrenTable
+        ],
+      )),
     );
   }
 }
