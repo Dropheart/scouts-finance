06ebe96992106eef785ec646154f75b958f56bc7
George Decesare <george.decesare@imperial.ic.ac.uk>
2025-06-04 21:47:12 +0100

    fix: move extension method to flutter project where it belongs

--- scouts_finances_flutter/lib/extensions/payment_method.dart
+++ scouts_finances_flutter/lib/extensions/payment_method.dart
@@ -2,7 +2,7 @@ import 'package:scouts_finances_client/scouts_finances_client.dart';
 
 extension PaymentMethodToString on PaymentMethod {
   // Converts a string to a PaymentMethod enum value.
-  String toDiplayString() {
+  String toDisplayString() {
     switch (this) {
       case PaymentMethod.cash:
         return 'Cash';
