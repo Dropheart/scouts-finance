b8c15607a9dce56d502571ed779b0919e3c5ed2d
Jay <jay@abussaud.dev>
2025-06-05 17:42:56 +0100

    feat: added "select child" by parent

--- scouts_finances_flutter/lib/payments/single_payment.dart
+++ scouts_finances_flutter/lib/payments/single_payment.dart
@@ -3,6 +3,7 @@ import 'package:scouts_finances_client/scouts_finances_client.dart';
 import 'package:scouts_finances_flutter/main.dart';
 import 'package:scouts_finances_flutter/payments/payment_table.dart';
 import 'package:scouts_finances_flutter/shared/parent_dropdown.dart';
+import 'package:scouts_finances_flutter/shared/text_dropdown.dart';
 
 class SinglePaymentView extends StatefulWidget {
   final int paymentId;
@@ -16,14 +17,15 @@ class _SinglePaymentViewState extends State<SinglePaymentView> {
   late Payment? payment;
   late List<Parent> parents;
   Parent get currParent => parents[parentIndex];
-  int parentIndex = 0;
-  int loading = 2; // Number of async operations to wait for
+  int parentIndex = -1;
+  int loading = 3; // Number of async operations to wait for
 
   @override
   void initState() {
     super.initState();
     _getPayment();
     _getParents();
+    _getScouts();
   }
 
   void _getPayment() async {
@@ -53,6 +55,16 @@ class _SinglePaymentViewState extends State<SinglePaymentView> {
     }
   }
 
+  late List<Child> scouts;
+  int scoutIndex = 0;
+  void _getScouts() async {
+    scouts = await client.scouts.getChildren();
+    scouts.sort((a, b) => a.lastName.compareTo(b.lastName));
+    setState(() {
+      loading = loading - 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     Widget body;
@@ -73,12 +85,54 @@ class _SinglePaymentViewState extends State<SinglePaymentView> {
       if (payment!.parentId != null) {
         // Then there is already a parent assigned so the index should reflect that
         parentIndex = parents.indexWhere((p) => p.id == payment!.parentId!);
-      } else {
+      } else if (parentIndex == -1) {
         // Try to find the parent by payee name
         parentIndex = parents.indexWhere((p) =>
             payment!.payee.contains(p.firstName) &&
             payment!.payee.contains(p.lastName));
-        if (parentIndex == -1) parentIndex = 0; // Default to first parent
+        parentIndex = 0; // Default to first parent
+      }
+
+      Row parentSelection = Row(children: [
+        Text("Assign this payment to parent: "),
+        ParentDropdown(
+          parents: parents,
+          defaultParentId: parents[parentIndex].id,
+          onChanged: (p) {
+            setState(() {
+              parentIndex = parents.indexWhere((parent) => parent.id == p);
+              scoutIndex = 0; // Reset scout index when parent changes
+            });
+          },
+        )
+      ]);
+
+      List<Child> filteredScouts =
+          scouts.where((s) => s.parentId == parents[parentIndex].id).toList();
+
+      Widget scoutSelection;
+      if (filteredScouts.isEmpty) {
+        scoutSelection = Center(
+          child: Text(
+            "No scouts found for this parent. Please assign a scout to this parent before classifying payments.",
+            style: const TextStyle(color: Colors.red, fontSize: 16),
+          ),
+        );
+      } else {
+        scoutSelection = Row(children: [
+          Text("Assign this payment to scout: "),
+          TextDropdown(
+              onChanged: (s) {
+                setState(() {
+                  scoutIndex =
+                      filteredScouts.indexWhere((scout) => scout.id == s);
+                });
+              },
+              values: filteredScouts,
+              valueToId: (s) => s.id!,
+              valueToString: (s) => '${s.firstName} ${s.lastName}',
+              defaultValue: filteredScouts[scoutIndex].id)
+        ]);
       }
 
       body = Column(
@@ -87,21 +141,7 @@ class _SinglePaymentViewState extends State<SinglePaymentView> {
         children: [
           PaymentTable(payment: payment!),
           const SizedBox(height: 16),
-          Row(
-            children: [
-              Text("Assign this payment to: "),
-              ParentDropdown(
-                parents: parents,
-                defaultParentId: parents[parentIndex].id,
-                onChanged: (p) {
-                  setState(() {
-                    parentIndex =
-                        parents.indexWhere((parent) => parent.id == p);
-                  });
-                },
-              )
-            ],
-          ),
+          Column(children: [parentSelection, scoutSelection]),
           const SizedBox(height: 32),
           Text(
               "This will change ${currParent.firstName}'s balance from -£10.00 to -£5.00"),
