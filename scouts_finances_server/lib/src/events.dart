7d838cdcf34e9e0c63e79db9dcfe92c85a267133
George Decesare <george.decesare@imperial.ic.ac.uk>
2025-06-06 03:23:52 +0100

    format

--- scouts_finances_server/lib/src/events.dart
+++ scouts_finances_server/lib/src/events.dart
@@ -17,8 +17,7 @@ class EventEndpoint extends Endpoint {
 
     final eventRegistration = await EventRegistration.db.find(session,
         where: (t) => t.eventId.equals(id),
-        include: EventRegistration.include(
-            child: Child.include()));
+        include: EventRegistration.include(child: Child.include()));
 
     return (eventDetails, eventRegistration);
   }
