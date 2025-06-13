f47147387c5a589184ab514b9ea1f76735741f15
George Decesare <george.decesare@imperial.ic.ac.uk>
2025-06-13 01:41:05 +0100

    fix: Consolidate events date format

--- scouts_finances_flutter/lib/events/home.dart
+++ scouts_finances_flutter/lib/events/home.dart
@@ -91,7 +91,7 @@ class _EventHomeState extends State<EventHome> {
               const Spacer(),
               Icon(Icons.calendar_today, size: 14),
               const SizedBox(width: 4.0),
-              Text('${event.date.day}/${event.date.month}/${event.date.year}'),
+              Text(event.date.toLocal().toIso8601String().split('T')[0]),
             ],
           ),
           onTap: () {
