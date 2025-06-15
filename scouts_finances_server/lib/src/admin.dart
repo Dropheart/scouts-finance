2e8cb087b14bd66f3086c81eb2392c5ca0838471
Jay <jay@abussaud.dev>
2025-06-15 06:15:46 +0100

    chore: wrap resetDb in transaction

--- scouts_finances_server/lib/src/admin.dart
+++ scouts_finances_server/lib/src/admin.dart
@@ -66,213 +66,233 @@ class AdminEndpoint extends Endpoint {
     final List<UserInfo> childInfos = (childJson['results'] as List)
         .map((c) => UserInfo.fromJson(c as Map<String, dynamic>))
         .toList();
+    httpClient.close();
 
-    // First, delete all existing data
-    await Payment.db.deleteWhere(session, where: (t) => t.id > -1);
-    await EventRegistration.db.deleteWhere(session, where: (t) => t.id > -1);
-    await Child.db.deleteWhere(session, where: (t) => t.id > -1);
-    await Event.db.deleteWhere(session, where: (t) => t.id > -1);
-    await Parent.db.deleteWhere(session, where: (t) => t.id > -1);
-    await BankAccount.db.deleteWhere(session, where: (t) => t.id > -1);
-    await ScoutGroup.db.deleteWhere(session, where: (t) => t.id > -1);
+    // Do it all in a transaction for performance + rollback
+    await session.db.transaction((t) async {
+      // First, delete all existing data
+      await Payment.db
+          .deleteWhere(session, where: (t) => t.id > -1, transaction: t);
+      await EventRegistration.db
+          .deleteWhere(session, where: (t) => t.id > -1, transaction: t);
+      await Child.db
+          .deleteWhere(session, where: (t) => t.id > -1, transaction: t);
+      await Event.db
+          .deleteWhere(session, where: (t) => t.id > -1, transaction: t);
+      await Parent.db
+          .deleteWhere(session, where: (t) => t.id > -1, transaction: t);
+      await BankAccount.db
+          .deleteWhere(session, where: (t) => t.id > -1, transaction: t);
+      await ScoutGroup.db
+          .deleteWhere(session, where: (t) => t.id > -1, transaction: t);
 
-    // Create scout groups
-    final groups =
-        scoutGroups.map((groupName) => ScoutGroup(name: groupName)).toList();
-    final insertedGroups = await ScoutGroup.db.insert(session, groups);
+      // Create scout groups
+      final groups =
+          scoutGroups.map((groupName) => ScoutGroup(name: groupName)).toList();
+      final insertedGroups =
+          await ScoutGroup.db.insert(session, groups, transaction: t);
 
-    // Create parents
-    final parents = <Parent>[];
-    for (var i = 0; i < numParents; i++) {
-      final info = parentInfos[i];
-      parents.add(Parent(
-        firstName: info.firstName,
-        lastName: info.lastName,
-        email: info.email!,
-        phone: info.phone!,
-        balance: 0,
-      ));
-    }
-    final insertedParents = await Parent.db.insert(session, parents);
-
-    // For each scout group, create & add children
-    final children = <Child>[];
-    for (final group in insertedGroups) {
-      int i = 0;
-      for (; i < childrenPerGroup; i++) {
-        final info = childInfos[i];
-        // Randomly assign a parent to the child
-        final parent =
-            insertedParents[Random().nextInt(insertedParents.length)];
-
-        children.add(Child(
+      // Create parents
+      final parents = <Parent>[];
+      for (var i = 0; i < numParents; i++) {
+        final info = parentInfos[i];
+        parents.add(Parent(
           firstName: info.firstName,
           lastName: info.lastName,
-          scoutGroupId: group.id!,
-          parentId: parent.id!,
+          email: info.email!,
+          phone: info.phone!,
+          balance: 0,
         ));
       }
-    }
-    final insertedChildren = await Child.db.insert(session, children);
+      final insertedParents =
+          await Parent.db.insert(session, parents, transaction: t);
 
-    // Create some events
-    final scoutEvents = [
-      'Rafting',
-      'Hiking',
-      'Camping',
-      'Swimming',
-      'Climbing',
-      'Archery',
-      'Cycling',
-      'Kayaking',
-      'Sailing',
-      'Rock Climbing',
-      'Caving',
-      'Fishing',
-      'Skiing',
-      'Snowboarding',
-      'Surfing',
-      'Horse Riding'
-    ];
+      // For each scout group, create & add children
+      final children = <Child>[];
+      for (final group in insertedGroups) {
+        int i = 0;
+        for (; i < childrenPerGroup; i++) {
+          final info = childInfos[i];
+          // Randomly assign a parent to the child
+          final parent =
+              insertedParents[Random().nextInt(insertedParents.length)];
 
-    // Each scout group gets between 3 and 7 events
-    final events = <Event>[];
-    for (final group in insertedGroups) {
-      final numEvents = 3 + Random().nextInt(5); // Between 3 and 7 events
-      final shuffledEvents = scoutEvents.toList()
-        ..shuffle()
-        ..take(numEvents);
-      for (var i = 0; i < numEvents; i++) {
-        final eventName = shuffledEvents[i];
-        final cost = 5 + Random().nextInt(2000); // Cost between 5 and 25 pounds
-        // Some time in the next year
-        final date = DateTime.now().add(Duration(days: Random().nextInt(365)));
-        events.add(Event(
-          name: eventName,
-          cost: cost,
-          date: date,
-          scoutGroupId: group.id!,
-        ));
+          children.add(Child(
+            firstName: info.firstName,
+            lastName: info.lastName,
+            scoutGroupId: group.id!,
+            parentId: parent.id!,
+          ));
+        }
       }
-    }
-    final insertedEvents = await Event.db.insert(session, events);
+      final insertedChildren =
+          await Child.db.insert(session, children, transaction: t);
 
-    // Register some children for events
-    // Each child has a 50% chance of being registered for a given event
-    final eventRegs = <(int childId, int eventId)>[];
-    for (final group in insertedGroups) {
-      final groupEvents =
-          insertedEvents.where((e) => e.scoutGroupId == group.id!).toList();
-      final groupChildren =
-          insertedChildren.where((c) => c.scoutGroupId == group.id!).toList();
+      // Create some events
+      final scoutEvents = [
+        'Rafting',
+        'Hiking',
+        'Camping',
+        'Swimming',
+        'Climbing',
+        'Archery',
+        'Cycling',
+        'Kayaking',
+        'Sailing',
+        'Rock Climbing',
+        'Caving',
+        'Fishing',
+        'Skiing',
+        'Snowboarding',
+        'Surfing',
+        'Horse Riding'
+      ];
 
-      for (final event in groupEvents) {
-        // Randomly decide whether to register the child for each event
-        for (final child in groupChildren) {
-          if (Random().nextBool()) {
-            eventRegs.add((child.id!, event.id!));
+      // Each scout group gets between 3 and 7 events
+      final events = <Event>[];
+      for (final group in insertedGroups) {
+        final numEvents = 3 + Random().nextInt(5); // Between 3 and 7 events
+        final shuffledEvents = scoutEvents.toList()
+          ..shuffle()
+          ..take(numEvents);
+        for (var i = 0; i < numEvents; i++) {
+          final eventName = shuffledEvents[i];
+          final cost =
+              5 + Random().nextInt(2000); // Cost between 5 and 25 pounds
+          // Some time in the next year
+          final date =
+              DateTime.now().add(Duration(days: Random().nextInt(365)));
+          events.add(Event(
+            name: eventName,
+            cost: cost,
+            date: date,
+            scoutGroupId: group.id!,
+          ));
+        }
+      }
+      final insertedEvents =
+          await Event.db.insert(session, events, transaction: t);
+
+      // Register some children for events
+      // Each child has a 50% chance of being registered for a given event
+      final eventRegs = <(int childId, int eventId)>[];
+      for (final group in insertedGroups) {
+        final groupEvents =
+            insertedEvents.where((e) => e.scoutGroupId == group.id!).toList();
+        final groupChildren =
+            insertedChildren.where((c) => c.scoutGroupId == group.id!).toList();
+
+        for (final event in groupEvents) {
+          // Randomly decide whether to register the child for each event
+          for (final child in groupChildren) {
+            if (Random().nextBool()) {
+              eventRegs.add((child.id!, event.id!));
+            }
           }
         }
       }
-    }
 
-    // Use the endpoint in case logic changes in the future
-    final endpoint = EventEndpoint();
-    for (final (childId, eventId) in eventRegs) {
-      await endpoint.registerChildForEvent(session, eventId, childId);
-    }
-
-    // Get IDs of the inserted registrations
-    final insertedRegistrations = await EventRegistration.db.find(session,
-        include: EventRegistration.include(
-            event: Event.include(),
-            child: Child.include(parent: Parent.include())));
-
-    // Create some payments for the registrations
-    final payments = <Payment>[];
-    for (final registration in insertedRegistrations) {
-      // Get the event for the event cost
-      final event = registration.event!;
-
-      // Randomly decide whether to pay
-      // All events are in the next year so decide
-      // based on how far in the future the event is
-      if (Random().nextDouble() >
-          (event.date.difference(DateTime.now()).inDays / 365)) {
-        continue; // Skip this registration, no payment
+      // Use the endpoint in case logic changes in the future
+      final endpoint = EventEndpoint();
+      for (final (childId, eventId) in eventRegs) {
+        await endpoint.registerChildForEvent(session, eventId, childId);
       }
 
-      // Randomly decide whether to pay in full or partially
-      // 80/20 split
-      final payInFull = Random().nextDouble() < 0.8;
+      // Get IDs of the inserted registrations
+      final insertedRegistrations = await EventRegistration.db.find(session,
+          include: EventRegistration.include(
+              event: Event.include(),
+              child: Child.include(parent: Parent.include())),
+          transaction: t);
 
-      // Randomly decide the amount to pay if not paying in full
-      final int amount =
-          payInFull ? event.cost : (event.cost * Random().nextDouble()).toInt();
-      // Randomly decide to pay in two installments or not - this bit is
-      // mainly to test our payment logic but also to simulate real-world
-      final payInTwoInstallments = Random().nextBool();
+      // Create some payments for the registrations
+      final payments = <Payment>[];
+      for (final registration in insertedRegistrations) {
+        // Get the event for the event cost
+        final event = registration.event!;
 
-      // Create the payments
-      final parent = registration.child!.parent!;
-      final payee = '${parent.firstName} ${parent.lastName}';
+        // Randomly decide whether to pay
+        // All events are in the next year so decide
+        // based on how far in the future the event is
+        if (Random().nextDouble() >
+            (event.date.difference(DateTime.now()).inDays / 365)) {
+          continue; // Skip this registration, no payment
+        }
 
-      // Nonsense ref because parents are a pain to deal with
-      // Eventually, change this to be X% chance of expected ref.
-      // For now, 18 characters of random alphanumeric string
-      String ref() => String.fromCharCodes(
-            List.generate(18, (index) => Random().nextInt(26) + 97),
+        // Randomly decide whether to pay in full or partially
+        // 80/20 split
+        final payInFull = Random().nextDouble() < 0.8;
+
+        // Randomly decide the amount to pay if not paying in full
+        final int amount = payInFull
+            ? event.cost
+            : (event.cost * Random().nextDouble()).toInt();
+        // Randomly decide to pay in two installments or not - this bit is
+        // mainly to test our payment logic but also to simulate real-world
+        final payInTwoInstallments = Random().nextBool();
+
+        // Create the payments
+        final parent = registration.child!.parent!;
+        final payee = '${parent.firstName} ${parent.lastName}';
+
+        // Nonsense ref because parents are a pain to deal with
+        // Eventually, change this to be X% chance of expected ref.
+        // For now, 18 characters of random alphanumeric string
+        String ref() => String.fromCharCodes(
+              List.generate(18, (index) => Random().nextInt(26) + 97),
+            );
+
+        final newPayments = payInTwoInstallments
+            ? [
+                Payment(
+                  reference: ref(),
+                  payee: payee,
+                  method: PaymentMethod.bank_transfer,
+                  amount: amount ~/ 2,
+                  date: DateTime.now().subtract(Duration(days: 15)),
+                ),
+                Payment(
+                  reference: ref(),
+                  payee: payee,
+                  method: PaymentMethod.bank_transfer,
+                  amount: amount ~/ 2,
+                  date: DateTime.now(),
+                ),
+              ]
+            : [
+                Payment(
+                  reference: ref(),
+                  payee: payee,
+                  method: PaymentMethod.bank_transfer,
+                  amount: amount,
+                  date: DateTime.now(),
+                ),
+              ];
+
+        payments.addAll(newPayments);
+      }
+
+      // Insert the payments
+      final insertedPayments =
+          await Payment.db.insert(session, payments, transaction: t);
+
+      // Assign ~20% of payments to parents
+      final assignedPayments = <(Payment payment, Parent parent)>[];
+      for (final payment in insertedPayments) {
+        if (Random().nextDouble() < 0.2) {
+          // Randomly assign a parent to the payment
+          final parent = insertedParents.firstWhere(
+            (p) => payment.payee == '${p.firstName} ${p.lastName}',
           );
 
-      final newPayments = payInTwoInstallments
-          ? [
-              Payment(
-                reference: ref(),
-                payee: payee,
-                method: PaymentMethod.bank_transfer,
-                amount: amount ~/ 2,
-                date: DateTime.now().subtract(Duration(days: 15)),
-              ),
-              Payment(
-                reference: ref(),
-                payee: payee,
-                method: PaymentMethod.bank_transfer,
-                amount: amount ~/ 2,
-                date: DateTime.now(),
-              ),
-            ]
-          : [
-              Payment(
-                reference: ref(),
-                payee: payee,
-                method: PaymentMethod.bank_transfer,
-                amount: amount,
-                date: DateTime.now(),
-              ),
-            ];
-
-      payments.addAll(newPayments);
-    }
-
-    // Insert the payments
-    final insertedPayments = await Payment.db.insert(session, payments);
-
-    // Assign ~20% of payments to parents
-    final assignedPayments = <(Payment payment, Parent parent)>[];
-    for (final payment in insertedPayments) {
-      if (Random().nextDouble() < 0.2) {
-        // Randomly assign a parent to the payment
-        final parent = insertedParents.firstWhere(
-          (p) => payment.payee == '${p.firstName} ${p.lastName}',
-        );
-
-        assignedPayments.add((payment, parent));
+          assignedPayments.add((payment, parent));
+        }
       }
-    }
-    // Use the API endpoint for the logic.
-    final paymentEndpoint = PaymentEndpoint();
-    for (final (payment, parent) in assignedPayments) {
-      await paymentEndpoint.updatePayment(session, payment.id!, parent);
-    }
+      // Use the API endpoint for the logic.
+      final paymentEndpoint = PaymentEndpoint();
+      for (final (payment, parent) in assignedPayments) {
+        await paymentEndpoint.updatePayment(session, payment.id!, parent);
+      }
+    });
   }
 }
